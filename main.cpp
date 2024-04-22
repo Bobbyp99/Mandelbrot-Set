398b31f1451fea0822f92bd2d3cf485354970bc0
Bobbyp99 <166642318+Bobbyp99@users.noreply.github.com>
2024-04-22 15:47:17 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <sstream>
+#include <vector>
 
+using namespace std;
+
+int main()
+{
+  VideoMode vm(VideoMode::getDesktopMode().width / 2,  VideoMode::getDesktopMode().length / 2);
+
+  RenderWindow window(vm, "Mandelbrot Set", Style::Default);
+}
