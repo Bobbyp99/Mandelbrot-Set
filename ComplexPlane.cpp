c598b7fba7af5b31e009dc470ffb8c89527d63f7
Bobbyp99 <166642318+Bobbyp99@users.noreply.github.com>
2024-11-24 15:35:21 -0800

    Update ComplexPlane.cpp

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -8,4 +8,6 @@ ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
     m_plane_size = {BASE_WIDTH, BASE_HEIGHT * m_aspectRatio};
     m_zoomCount = 0;
     m_State = State::CALCULATING;
+    m_vArray.setPrimitiveType(Points);
+    m_vArray.resize(pixelWidth * pixelHeight);
 }
