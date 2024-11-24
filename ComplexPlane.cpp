f93236dee974bed102e8032e9e668523ab33e1e2
Bobbyp99 <166642318+Bobbyp99@users.noreply.github.com>
2024-11-24 15:45:46 -0800

    Update ComplexPlane.cpp

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -11,3 +11,32 @@ ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
     m_vArray.setPrimitiveType(Points);
     m_vArray.resize(pixelWidth * pixelHeight);
 }
+
+void ComplexPlane::draw(RenderTarget& target, RenderStates states) const
+{
+    target.draw(m_vArray);
+}
+
+void complexPlane::updateRender()
+{
+    if(m_State == State::CALCULATING)
+    {
+        for(int i = 0; i < pixel.Height; ++i)
+        {
+            for(int j = 0; i < pixel.Width; ++j)  
+            {
+                
+            }
+        }
+    }
+}
+
+void ComplexPlane::zoomIn()
+{
+    ++m_zoomCount;
+    float x = BASE_WIDTH * (pow(BASE_ZOOM, m_ZoomCount));
+    float y = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_ZoomCount));
+    m_plane_size = {x, y};
+    m_State = State::CALCULATING;
+}
+
