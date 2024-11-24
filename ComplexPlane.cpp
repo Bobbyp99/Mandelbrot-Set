17f5c255462d0604e4c94b551edec2edebc221f8
Bobbyp99 <166642318+Bobbyp99@users.noreply.github.com>
2024-11-24 15:30:18 -0800

    Update ComplexPlane.cpp

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -1,2 +1,11 @@
 #include "ComplexPlane.h"
 
+ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
+{
+    m_pixel_size = {pixelWidth, pixelHeight};
+    m_aspectRatio = pixelHeight / static_cast<float> pixelWidth;
+    m_plane_center = {0, 0};
+    m_plane_size = {BASE_WIDTH, BASE_HEIGHT * m_aspectRatio};
+    m_zoomCount = 0;
+    m_State = State::CALCULATING;
+}
